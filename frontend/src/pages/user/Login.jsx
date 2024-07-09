86782eb9b9f4079b9b4ec1e4432c361e44c6d5e8
Debaraj Malik <debarajmalik@outlook.com>
2024-07-09 10:43:53 +0530

    main

--- frontend/src/pages/user/Login.jsx
+++ frontend/src/pages/user/Login.jsx
@@ -91,11 +91,7 @@ const Login = () => {
                         <span className="p-2 text-gray-400">OR</span>
                         <hr className="w-full border-gray-300" />
                     </div>
-<<<<<<< HEAD
-                    {/* <div className="grid grid-cols-1 gap-4 mt-6">
-=======
-{/*                     <div className="grid grid-cols-1 gap-4 mt-6">
->>>>>>> 9cc152d59aae4c670d2acb5b7118d0b8fd66ebc5
+                    <div className="grid grid-cols-1 gap-4 mt-6">
                         <button onClick={() => toast.error('Admin says: This feature is in progress!! try manual Login')} className="flex items-center justify-center bg-red-500 text-white py-2 rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600">
                             <FontAwesomeIcon icon={faGoogle} className="mr-2" />
                             Login with Google
@@ -104,7 +100,7 @@ const Login = () => {
                             <FontAwesomeIcon icon={faFacebook} className="mr-2" />
                             Login with Facebook
                         </button>
-                    </div> */}
+                    </div>
                     <div className="text-center mt-6 text-sm text-gray-600">
                         Don't have an account? <a href="/register" className="text-blue-500 hover:underline">Register</a>
                     </div>
--- frontend/src/pages/user/Register.jsx
+++ frontend/src/pages/user/Register.jsx
@@ -76,11 +76,7 @@ const Register = () => {
                     <span className="p-2 text-gray-400">OR</span>
                     <hr className="w-full border-gray-300" />
                 </div>
-<<<<<<< HEAD
-                {/* <div className="grid grid-cols-1 gap-4 mt-6">
-=======
-{/*                 <div className="grid grid-cols-1 gap-4 mt-6">
->>>>>>> 9cc152d59aae4c670d2acb5b7118d0b8fd66ebc5
+                <div className="grid grid-cols-1 gap-4 mt-6">
                     <button onClick={() => toast.error('Admin says: This feature is in progress!! try manual Register')} className="flex items-center justify-center bg-red-500 text-white py-2 rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600">
                         <FontAwesomeIcon icon={faGoogle} className="mr-2" />
                         Register with Google
@@ -89,7 +85,7 @@ const Register = () => {
                         <FontAwesomeIcon icon={faFacebook} className="mr-2" />
                         Register with Facebook
                     </button>
-                </div> */}
+                </div>
                 <div className="text-center mt-6 text-sm text-gray-600">
                     Already have an account? <a href="/login" className="text-blue-500 hover:underline">Login</a>
                 </div>
